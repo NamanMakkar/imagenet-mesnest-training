55ca6d50ae5fc7076366c6f83c04f484ee3f56c3
NamanMakkar <49094639+NamanMakkar@users.noreply.github.com>
2022-03-27 22:21:50 +0530

    Add files via upload

--- training/mesnest.py
+++ training/mesnest.py
@@ -110,26 +110,26 @@ class SplitAttn(nn.Module):
             attn_chs = rd_channels * radix
 
         padding = kernel_size // 2 if padding is None else padding
-        #self.conv = nn.Conv2d(
-        #   c1, mid_chs, kernel_size, stride, padding, dilation,
-        #    groups=groups * radix, bias=bias, **kwargs)
-        self.conv = GhostConv(c1,mid_chs,k=kernel_size,s=stride,g=groups*radix)
-        #self.bn0 = norm_layer(mid_chs) if norm_layer else nn.Identity()
-        #self.act0 = act_layer()
-        #self.fc1 = nn.Conv2d(c2, attn_chs, 1, groups=groups)
-        self.fc1 = GhostConv(c2, attn_chs, 1, 1)
-        #self.bn1 = norm_layer(attn_chs) if norm_layer else nn.Identity()
-        #self.act1 = act_layer()
-        #self.fc2 = nn.Conv2d(attn_chs, mid_chs, 1, groups=groups)
-        self.fc2 = GhostConv(attn_chs, mid_chs, 1, 1, act=False)
+        self.conv = nn.Conv2d(
+           c1, mid_chs, kernel_size, stride, padding, dilation,
+            groups=groups * radix, bias=bias, **kwargs)
+        #self.conv = GhostConv(c1,mid_chs,k=kernel_size,s=stride,g=groups*radix)
+        self.bn0 = norm_layer(mid_chs) if norm_layer else nn.Identity()
+        self.act0 = act_layer()
+        self.fc1 = nn.Conv2d(c2, attn_chs, 1, groups=groups)
+        #self.fc1 = GhostConv(c2, attn_chs, 1, 1)
+        self.bn1 = norm_layer(attn_chs) if norm_layer else nn.Identity()
+        self.act1 = act_layer()
+        self.fc2 = nn.Conv2d(attn_chs, mid_chs, 1, groups=groups)
+        #self.fc2 = GhostConv(attn_chs, mid_chs, 1, 1, act=False)
         self.rsoftmax = rSoftMax(radix, groups)
 
     def forward(self, x):
         x = self.conv(x)
-        #x = self.bn0(x)
+        x = self.bn0(x)
         if self.drop_block is not None:
             x = self.drop_block(x)
-        #x = self.act0(x)
+        x = self.act0(x)
 
         B, RC, H, W = x.shape
         if self.radix > 1:
@@ -139,8 +139,8 @@ class SplitAttn(nn.Module):
             x_gap = x
         x_gap = x_gap.mean((2, 3), keepdim=True)
         x_gap = self.fc1(x_gap)
-        #x_gap = self.bn1(x_gap)
-        #x_gap = self.act1(x_gap)
+        x_gap = self.bn1(x_gap)
+        x_gap = self.act1(x_gap)
         x_attn = self.fc2(x_gap)
 
         x_attn = self.rsoftmax(x_attn).view(B, -1, 1, 1)
